6cac909b22e8d89f90e996838e643e695de7ec2a
juansm1664 <juansm3007@gmail.com>
2024-04-17 18:00:21 -0500

    ArquitectHexagonal

---
+++ src/main/java/com/juandavid/project/arquiHexagonal/Application.java
@@ -0,0 +1,13 @@
+package com.juandavid.project.arquiHexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
